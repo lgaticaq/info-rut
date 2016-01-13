46a258ec35a297d14f64924dd8fe33e95254da7b
Leonardo Gatica <lgaticastyle@gmail.com>
2016-01-13 00:37:01 -0300

    Add getRut

--- src/index.js
+++ src/index.js
@@ -1,32 +1,72 @@
-'use strict'
+'use strict';
 
-import http from 'http'
-import cheerio from 'cheerio'
-import Q from 'q'
-import Rut from 'rutjs'
+import rp from 'request-promise';
+import cheerio from 'cheerio';
+import Q from 'q';
+import Rut from 'rutjs';
 
-export default (data, callback) => {
-  const deferred = Q.defer()
-  const _rut = new Rut(data)
-  const rut = _rut.isValid ? _rut.getNiceRut(false) : '1'
+const getFullName = (data, callback) => {
+  const deferred = Q.defer();
+  const _rut = new Rut(data);
+  const rut = _rut.isValid ? _rut.getNiceRut(false) : '1';
 
-  http.get(`http://datos.24x7.cl/rut/${rut}/`, (response) => {
-    let body = ''
+  const options = {
+    url: `http://datos.24x7.cl/rut/${rut}/`,
+    transform: (body) => cheerio.load(body)
+  };
+  rp(options).then($ => {
+    const fullName = $('h1:contains(Nombre)').next().text();
+    if (fullName === '') {
+      deferred.reject(new Error('Not found full name'));
+    } else {
+      deferred.resolve(fullName);
+    }
+  }).catch(deferred.reject);
 
-    response.on('data', (chunk) => body += chunk)
+  deferred.promise.nodeify(callback);
 
-    response.on('end', () => {
-      const $ = cheerio.load(body)
-      const fullName = $('h1:contains(Nombre)').next().text()
-      if (fullName === '') {
-        deferred.reject(new Error('Not found full name'))
-      } else {
-        deferred.resolve(fullName)
+  return deferred.promise;
+};
+
+const getRut = (name, callback) => {
+  const deferred = Q.defer();
+  const options = {
+    url: `http://datos.24x7.cl/`,
+    transform: (body) => cheerio.load(body)
+  };
+  const rpap = rp.defaults({jar: true});
+  rpap(options).then($ => {
+    const csrf = $('input[name=csrfmiddlewaretoken]').val();
+    const options = {
+      method: 'POST',
+      url: 'http://datos.24x7.cl/get_generic_ajax/',
+      form: {
+        entrada: name,
+        csrfmiddlewaretoken: csrf
+      },
+      json: true
+    };
+    rpap(options).then(data => {
+      let results = [];
+      if (data.status === 'success') {
+        results = data.value.map(x => {
+          return {
+            url: `http://datos.24x7.cl/rut/${x.rut}/`,
+            fullName: x.name,
+            rut: x.rut
+          };
+        });
       }
-    })
-  }).on('error', (err) => deferred.reject(err))
+      deferred.resolve(results);
+    });
+  }).catch(deferred.reject);
 
-  deferred.promise.nodeify(callback)
+  deferred.promise.nodeify(callback);
 
-  return deferred.promise
-}
+  return deferred.promise;
+};
+
+module.exports = {
+  getFullName: getFullName,
+  getRut: getRut
+};
