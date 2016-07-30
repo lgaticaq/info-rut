0d7bdf265236ff22bf9dd2902f31228a24628077
Leonardo Gatica <lgatica@protonmail.com>
2016-07-30 00:15:22 -0400

    Update example

--- example.js
+++ example.js
@@ -3,5 +3,5 @@ const infoRut = require('info-rut');
 const rut = '11111111-1';
 const name = 'juan perez';
 
-const fullName = await infoRut.getFullName(rut);
-const results = await infoRut.getRut(name);
+// await infoRut.getFullName(rut);
+await infoRut.getRut(name);
