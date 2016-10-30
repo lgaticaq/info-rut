f4c4d753aef78ac77c79e313d4f16375b22b656a
Leonardo Gatica <lgatica@protonmail.com>
2016-10-30 01:07:29 -0300

    feat(rut): Split search person and enterprise rut.
    Add fuzzy search.
    Fix domain.
    Better format name and rut.
    Add new tests.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const Rut = require('rutjs');
+const Fuse = require('fuse.js');
+
+const options = {transform: cheerio.load};
+const domain = 'http://chile.rutificador.com';
 
 const getFullName = data => {
   const _rut = new Rut(data);
   const rut = _rut.isValid ? _rut.getNiceRut(false) : '1';
-
-  const options = {
-    url: `http://datos.24x7.cl/rut/${rut}/`,
-    transform: cheerio.load
-  };
+  options.url = `${domain}/rut/${rut}/`;
   return rp(options).then($ => {
     const fullName = $('h1:contains(Nombre)').next().text();
     if (fullName === '') throw new Error('Not found full name');
@@ -19,18 +19,38 @@ const getFullName = data => {
   });
 };
 
-const getRut = name => {
+const reverse = name => {
+  const words = name.split(' ');
+  if (words.length !== 4) return name;
+  return words.slice(2, 4).concat(words.slice(0, 2)).join(' ');
+};
+
+const titleize = name => {
+  return name.toLowerCase().replace(/(?:^|\s|-)\S/g, c => c.toUpperCase());
+};
+
+const fuzzzySearch = (name, list) => {
   const options = {
-    url: 'http://datos.24x7.cl/',
-    transform: cheerio.load
+    shouldSort: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    keys: ['fullName']
   };
+  const fuse = new Fuse(list, options);
+  return fuse.search(name);
+};
+
+const getRut = name => {
+  options.url = `${domain}/`;
   const rpap = rp.defaults({jar: true});
   return rpap(options)
     .then($ => $('input[name=csrfmiddlewaretoken]').val())
     .then(csrf => {
       const options = {
         method: 'POST',
-        url: 'http://datos.24x7.cl/get_generic_ajax/',
+        url: `${domain}/get_generic_ajax/`,
         form: {
           entrada: name,
           csrfmiddlewaretoken: csrf
@@ -44,9 +64,9 @@ const getRut = name => {
         results = data.value.map(x => {
           const rut = new Rut(x.rut.toString(), true);
           return {
-            url: `http://datos.24x7.cl/rut/${rut.getNiceRut(false)}/`,
-            fullName: x.name,
-            rut: rut.getNiceRut(false)
+            url: `${domain}/rut/${rut.getNiceRut(false)}/`,
+            fullName: titleize(x.name),
+            rut: rut.getNiceRut()
           };
         });
       }
@@ -54,7 +74,28 @@ const getRut = name => {
     });
 };
 
+const isEnterprise = rut => {
+  return rut.length === 12 && parseInt(rut[0], 10) > 6;
+};
+
+const getPersonRut = name => {
+  return getRut(name).then(results => {
+    const list = results.filter(x => !isEnterprise(x.rut)).map(x => {
+      return {url: x.url, fullName: reverse(x.fullName), rut: x.rut};
+    });
+    return fuzzzySearch(name, list);
+  });
+};
+
+const getEnterpriseRut = name => {
+  return getRut(name).then(results => {
+    const list = results.filter(x => isEnterprise(x.rut));
+    return fuzzzySearch(name, list);
+  });
+};
+
 module.exports = {
   getFullName: getFullName,
-  getRut: getRut
+  getPersonRut: getPersonRut,
+  getEnterpriseRut: getEnterpriseRut
 };
--- test/test.js
+++ test/test.js
@@ -5,47 +5,33 @@ const expect = require('chai').expect;
 const nock = require('nock');
 const lib = require('../src');
 
+const domain = 'http://chile.rutificador.com';
+
 describe('info-rut', () => {
-
-
   describe('getFullName valid rut', () => {
-
-    const rut = '11111111-1';
-
     beforeEach(() => {
       nock.disableNetConnect();
-      nock('http://datos.24x7.cl')
-        .get(`/rut/${rut}/`)
+      nock(domain)
+        .get('/rut/11111111-1/')
         .replyWithFile(200, path.join(__dirname, 'valid.html'));
     });
-
     it('should return a full name', done => {
-      lib.getFullName(rut).then(fullName => {
+      lib.getFullName('11111111-1').then(fullName => {
         expect(fullName).to.eql('Anonymous');
-        done();
-      }).catch(err => {
-        expect(err).to.be.null;
         done();
       });
     });
   });
 
   describe('getFullName invalid rut', () => {
-
-    const rut = '1';
-
     beforeEach(() => {
       nock.disableNetConnect();
-      nock('http://datos.24x7.cl')
-        .get(`/rut/${rut}/`)
+      nock(domain)
+        .get('/rut/1/')
         .replyWithFile(404, path.join(__dirname, 'invalid.html'));
     });
-
     it('should return a error', done => {
-      lib.getFullName(rut).then(fullName => {
-        expect(fullName).to.be.undefined;
-        done();
-      }).catch(err => {
+      lib.getFullName('1').catch(err => {
         var fn = function () { throw err; };
         expect(fn).to.throw(Error);
         done();
@@ -53,66 +39,107 @@ describe('info-rut', () => {
     });
   });
 
-  describe('getRut valid name', () => {
-
-    const name = 'perez';
-
+  describe('getFullName not found rut', () => {
     beforeEach(() => {
       nock.disableNetConnect();
-      const form = {
-        entrada: name,
-        csrfmiddlewaretoken: 'asdf'
-      };
-      nock('http://datos.24x7.cl')
+      nock(domain)
+        .get('/rut/11111111-1/')
+        .replyWithFile(200, path.join(__dirname, 'notfound.html'));
+    });
+    it('should return a error', done => {
+      lib.getFullName('11111111-1').catch(err => {
+        var fn = function () { throw err; };
+        expect(fn).to.throw(Error);
+        done();
+      });
+    });
+  });
+
+  describe('getPersonRut valid name', () => {
+    beforeEach(() => {
+      nock.disableNetConnect();
+      const form = {entrada: 'perez', csrfmiddlewaretoken: 'asdf'};
+      nock(domain)
         .get('/')
         .replyWithFile(200, path.join(__dirname, 'form.html'))
         .post('/get_generic_ajax/', form)
         .reply(200, {
           status: 'success',
           value: [
-            {name: 'JUAN PEREZ', rut: 11111111},
-            {name: 'PEDRO PEREZ', rut: 22222222}
+            {name: 'PEREZ SOTO JUAN PEDRO', rut: 11111111},
+            {name: 'PEREZ SOTO PEDRO', rut: 22222222}
           ]
         });
     });
-
-    it('should return a array of results (promise)', done => {
-      lib.getRut(name).then(results => {
+    it('should return a array of results', done => {
+      lib.getPersonRut('perez').then(results => {
         expect(results).to.eql([
-          {fullName: 'JUAN PEREZ', rut: '11111111-1', url: 'http://datos.24x7.cl/rut/11111111-1/'},
-          {fullName: 'PEDRO PEREZ', rut: '22222222-2', url: 'http://datos.24x7.cl/rut/22222222-2/'}
+          {fullName: 'Perez Soto Pedro', rut: '22.222.222-2', url: `${domain}/rut/22222222-2/`},
+          {fullName: 'Juan Pedro Perez Soto', rut: '11.111.111-1', url: `${domain}/rut/11111111-1/`}
         ]);
-        done();
-      }).catch(err => {
-        expect(err).to.be.null;
         done();
       });
     });
   });
 
-  describe('getRut invalid name', () => {
-
-    const name = 'asdf';
-
+  describe('getPersonRut invalid name', () => {
     beforeEach(() => {
       nock.disableNetConnect();
-      const form = {
-        entrada: name,
-        csrfmiddlewaretoken: 'asdf'
-      };
-      nock('http://datos.24x7.cl')
+      const form = {entrada: 'asdf', csrfmiddlewaretoken: 'asdf'};
+      nock(domain)
         .get('/')
         .replyWithFile(200, path.join(__dirname, 'form.html'))
         .post('/get_generic_ajax/', form)
         .reply(200, {status: 'fail', value: []});
     });
-
     it('should return a empty results', done => {
-      lib.getRut(name).then(results => {
+      lib.getPersonRut('asdf').then(results => {
         expect(results).to.eql([]);
         done();
-      }).catch(err => {
-        expect(err).to.be.null;
+      });
+    });
+  });
+
+  describe('getEnterpriseRut valid name', () => {
+    beforeEach(() => {
+      nock.disableNetConnect();
+      const form = {entrada: 'sushi', csrfmiddlewaretoken: 'asdf'};
+      nock(domain)
+        .get('/')
+        .replyWithFile(200, path.join(__dirname, 'form.html'))
+        .post('/get_generic_ajax/', form)
+        .reply(200, {
+          status: 'success',
+          value: [
+            {name: 'SUSHI CHILE', rut: 77777777},
+            {name: 'SUSHI SANTIAGO', rut: 88888888}
+          ]
+        });
+    });
+    it('should return a array of results', done => {
+      lib.getEnterpriseRut('sushi').then(results => {
+        expect(results).to.eql([
+          {fullName: 'Sushi Chile', rut: '77.777.777-7', url: `${domain}/rut/77777777-7/`},
+          {fullName: 'Sushi Santiago', rut: '88.888.888-8', url: `${domain}/rut/88888888-8/`}
+        ]);
+        done();
+      });
+    });
+  });
+
+  describe('getEnterpriseRut invalid name', () => {
+    beforeEach(() => {
+      nock.disableNetConnect();
+      const form = {entrada: 'asdf', csrfmiddlewaretoken: 'asdf'};
+      nock(domain)
+        .get('/')
+        .replyWithFile(200, path.join(__dirname, 'form.html'))
+        .post('/get_generic_ajax/', form)
+        .reply(200, {status: 'fail', value: []});
+    });
+    it('should return a empty results', done => {
+      lib.getEnterpriseRut('asdf').then(results => {
+        expect(results).to.eql([]);
         done();
       });
     });
