319dd4287263c05999eaf05069270c8dbbfa2e78
Leonardo Gatica <lgatica@protonmail.com>
2018-01-22 16:46:33 -0300

    feat(source): set new source to https://rutify.cl
    Set min node version to v8
    Migrate to standardjs
    Change rut lib to rut.js
    Update dependencies
    Remove yarn

--- src/index.js
+++ src/index.js
@@ -1,34 +1,62 @@
-'use strict';
+'use strict'
 
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const Rut = require('rutjs');
-const Fuse = require('fuse.js');
+const https = require('https')
+const querystring = require('querystring')
+const { validate, clean, format } = require('rut.js')
+const Fuse = require('fuse.js')
 
-const userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
-const options = {transform: cheerio.load, headers: {'User-Agent': userAgent}};
-const domain = 'https://chile.rutificador.com';
+const getData = data => {
+  return new Promise((resolve, reject) => {
+    const qs = querystring.stringify({ q: data })
+    const options = {
+      hostname: 'api.rutify.cl',
+      port: 443,
+      path: `/search?${qs}`,
+      method: 'GET'
+    }
+    const req = https.request(options, res => {
+      if (res.statusCode !== 200) {
+        reject(new Error(`Request Failed. Status Code: ${res.statusCode}`))
+      } else {
+        res.setEncoding('utf8')
+        let rawData = ''
+        res.on('data', chunk => {
+          rawData += chunk
+        })
+        res.on('end', () => {
+          try {
+            resolve(JSON.parse(rawData))
+          } catch (err) {
+            reject(err)
+          }
+        })
+      }
+    })
+    req.on('error', err => reject(err))
+    req.end()
+  })
+}
 
 const getFullName = data => {
-  const _rut = new Rut(data);
-  const rut = _rut.isValid ? _rut.getNiceRut(false) : '1';
-  options.url = `${domain}/rut/${rut}/`;
-  return rp(options).then($ => {
-    const fullName = $('h1:contains(Nombre)').next().text();
-    if (fullName === '') throw new Error('Not found full name');
-    return reverse(fullName.split(', Buscas').shift());
-  });
-};
+  const rut = validate(data) ? data : '1'
+  return getData(rut).then(data => {
+    if (data.length === 0) throw new Error('Not found full name')
+    return reverse(titleize(data[0].name))
+  })
+}
 
 const reverse = name => {
-  const words = name.split(' ');
-  if (words.length !== 4) return name;
-  return words.slice(2, 4).concat(words.slice(0, 2)).join(' ');
-};
+  const words = name.split(' ')
+  if (words.length !== 4) return name
+  return words
+    .slice(2, 4)
+    .concat(words.slice(0, 2))
+    .join(' ')
+}
 
 const titleize = name => {
-  return name.toLowerCase().replace(/(?:^|\s|-)\S/g, c => c.toUpperCase());
-};
+  return name.toLowerCase().replace(/(?:^|\s|-)\S/g, c => c.toUpperCase())
+}
 
 const fuzzzySearch = (name, list) => {
   const options = {
@@ -38,71 +66,45 @@ const fuzzzySearch = (name, list) => {
     distance: 100,
     maxPatternLength: 32,
     keys: ['fullName']
-  };
-  const fuse = new Fuse(list, options);
-  return fuse.search(name);
-};
+  }
+  const fuse = new Fuse(list, options)
+  return fuse.search(name)
+}
 
 const getRut = name => {
-  options.url = `${domain}/`;
-  const rpap = rp.defaults({jar: true});
-  return rpap(options)
-    .then($ => $('input[name=csrfmiddlewaretoken]').val())
-    .then(csrf => {
-      const options = {
-        method: 'POST',
-        url: `${domain}/get_generic_ajax/`,
-        form: {
-          csrfmiddlewaretoken: csrf,
-          entrada: name
-        },
-        json: true,
-        headers: {
-          'User-Agent': userAgent,
-          Origin: domain,
-          'X-Requested-With': 'XMLHttpRequest',
-          Referer: `${domain}/`
-        }
-      };
-      return rpap(options);
-    }).then(data => {
-      let results = [];
-      if (data.status === 'success') {
-        results = data.value.map(x => {
-          const rut = new Rut(x.rut.toString(), true);
-          return {
-            url: `${domain}/rut/${rut.getNiceRut(false)}/`,
-            fullName: titleize(x.name),
-            rut: rut.getNiceRut()
-          };
-        });
+  return getData(name).then(data => {
+    return data.map(result => {
+      return {
+        url: `https://rutify.cl/rut/${clean(result.rut.toString())}`,
+        fullName: titleize(result.name),
+        rut: format(result.rut.toString())
       }
-      return results;
-    });
-};
+    })
+  })
+}
 
 const isEnterprise = rut => {
-  return rut.length === 12 && parseInt(rut[0], 10) > 5;
-};
+  return rut.length === 12 && parseInt(rut[0], 10) > 5
+}
 
 const getPersonRut = name => {
   return getRut(name).then(results => {
     const list = results.filter(x => !isEnterprise(x.rut)).map(x => {
-      return {url: x.url, fullName: reverse(x.fullName), rut: x.rut};
-    });
-    return fuzzzySearch(name, list);
-  });
-};
+      return { url: x.url, fullName: reverse(x.fullName), rut: x.rut }
+    })
+    return fuzzzySearch(name, list)
+  })
+}
 
 const getEnterpriseRut = name => {
   return getRut(name).then(results => {
-    const list = results.filter(x => isEnterprise(x.rut));
-    return fuzzzySearch(name, list);
-  });
-};
+    const list = results.filter(x => isEnterprise(x.rut))
+    return fuzzzySearch(name, list)
+  })
+}
 
 module.exports = {
   getFullName: getFullName,
   getPersonRut: getPersonRut,
   getEnterpriseRut: getEnterpriseRut
-};
+}
--- test/test.js
+++ test/test.js
@@ -1,147 +1,165 @@
-'use strict';
+'use strict'
 
-const path = require('path');
-const expect = require('chai').expect;
-const nock = require('nock');
-const lib = require('../src');
-
-const domain = 'https://chile.rutificador.com';
+const { describe, it, beforeEach } = require('mocha')
+const { expect } = require('chai')
+const nock = require('nock')
+const lib = require('../src')
 
 describe('info-rut', () => {
   describe('getFullName valid rut', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      nock(domain)
-        .get('/rut/11111111-1/')
-        .replyWithFile(200, path.join(__dirname, 'valid.html'));
-    });
-    it('should return a full name', done => {
-      lib.getFullName('11111111-1').then(fullName => {
-        expect(fullName).to.eql('Anonymous');
-        done();
-      });
-    });
-  });
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: '11111111-1' })
+        .reply(200, [{ rut: '111111111', name: 'anonymous' }])
+    })
+    it('should return a full name', async () => {
+      const fullName = await lib.getFullName('11111111-1')
+      expect(fullName).to.eql('Anonymous')
+    })
+  })
 
   describe('getFullName invalid rut', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      nock(domain)
-        .get('/rut/1/')
-        .replyWithFile(404, path.join(__dirname, 'invalid.html'));
-    });
-    it('should return a error', done => {
-      lib.getFullName('1').catch(err => {
-        var fn = function () { throw err; };
-        expect(fn).to.throw(Error);
-        done();
-      });
-    });
-  });
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: '11111111-1' })
+        .reply(200, [])
+    })
+    it('should return a error', async () => {
+      try {
+        await lib.getFullName('1')
+      } catch (err) {
+        expect(err).to.be.an('error')
+      }
+    })
+  })
 
   describe('getFullName not found rut', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      nock(domain)
-        .get('/rut/11111111-1/')
-        .replyWithFile(200, path.join(__dirname, 'notfound.html'));
-    });
-    it('should return a error', done => {
-      lib.getFullName('11111111-1').catch(err => {
-        var fn = function () { throw err; };
-        expect(fn).to.throw(Error);
-        done();
-      });
-    });
-  });
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: '11111111-1' })
+        .reply(200, [])
+    })
+    it('should return a error', async () => {
+      try {
+        await lib.getFullName('11111111-1')
+      } catch (err) {
+        expect(err).to.be.an('error')
+      }
+    })
+  })
 
   describe('getPersonRut valid name', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      const form = {entrada: 'perez', csrfmiddlewaretoken: 'asdf'};
-      nock(domain)
-        .get('/')
-        .replyWithFile(200, path.join(__dirname, 'form.html'))
-        .post('/get_generic_ajax/', form)
-        .reply(200, {
-          status: 'success',
-          value: [
-            {name: 'PEREZ SOTO JUAN PEDRO', rut: 11111111},
-            {name: 'PEREZ SOTO PEDRO', rut: 22222222}
-          ]
-        });
-    });
-    it('should return a array of results', done => {
-      lib.getPersonRut('perez').then(results => {
-        expect(results).to.eql([
-          {fullName: 'Perez Soto Pedro', rut: '22.222.222-2', url: `${domain}/rut/22222222-2/`},
-          {fullName: 'Juan Pedro Perez Soto', rut: '11.111.111-1', url: `${domain}/rut/11111111-1/`}
-        ]);
-        done();
-      });
-    });
-  });
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: 'perez' })
+        .reply(200, [
+          { name: 'perez soto juan pedro', rut: '111111111' },
+          { name: 'perez soto pedro', rut: '222222222' }
+        ])
+    })
+    it('should return a array of results', async () => {
+      const results = await lib.getPersonRut('perez')
+      expect(results).to.eql([
+        {
+          fullName: 'Perez Soto Pedro',
+          rut: '22.222.222-2',
+          url: `https://rutify.cl/rut/222222222`
+        },
+        {
+          fullName: 'Juan Pedro Perez Soto',
+          rut: '11.111.111-1',
+          url: `https://rutify.cl/rut/111111111`
+        }
+      ])
+    })
+  })
 
   describe('getPersonRut invalid name', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      const form = {entrada: 'asdf', csrfmiddlewaretoken: 'asdf'};
-      nock(domain)
-        .get('/')
-        .replyWithFile(200, path.join(__dirname, 'form.html'))
-        .post('/get_generic_ajax/', form)
-        .reply(200, {status: 'fail', value: []});
-    });
-    it('should return a empty results', done => {
-      lib.getPersonRut('asdf').then(results => {
-        expect(results).to.eql([]);
-        done();
-      });
-    });
-  });
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: 'asdf' })
+        .reply(200, [])
+    })
+    it('should return a empty results', async () => {
+      const results = await lib.getPersonRut('asdf')
+      expect(results).to.eql([])
+    })
+  })
 
   describe('getEnterpriseRut valid name', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      const form = {entrada: 'sushi', csrfmiddlewaretoken: 'asdf'};
-      nock(domain)
-        .get('/')
-        .replyWithFile(200, path.join(__dirname, 'form.html'))
-        .post('/get_generic_ajax/', form)
-        .reply(200, {
-          status: 'success',
-          value: [
-            {name: 'SUSHI CHILE', rut: 77777777},
-            {name: 'SUSHI SANTIAGO', rut: 88888888}
-          ]
-        });
-    });
-    it('should return a array of results', done => {
-      lib.getEnterpriseRut('sushi').then(results => {
-        expect(results).to.eql([
-          {fullName: 'Sushi Chile', rut: '77.777.777-7', url: `${domain}/rut/77777777-7/`},
-          {fullName: 'Sushi Santiago', rut: '88.888.888-8', url: `${domain}/rut/88888888-8/`}
-        ]);
-        done();
-      });
-    });
-  });
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: 'sushi' })
+        .reply(200, [
+          { name: 'SUSHI CHILE', rut: '777777777' },
+          { name: 'SUSHI SANTIAGO', rut: '888888888' }
+        ])
+    })
+    it('should return a array of results', async () => {
+      const results = await lib.getEnterpriseRut('sushi')
+      expect(results).to.eql([
+        {
+          fullName: 'Sushi Chile',
+          rut: '77.777.777-7',
+          url: `https://rutify.cl/rut/777777777`
+        },
+        {
+          fullName: 'Sushi Santiago',
+          rut: '88.888.888-8',
+          url: `https://rutify.cl/rut/888888888`
+        }
+      ])
+    })
+  })
 
   describe('getEnterpriseRut invalid name', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
-      const form = {entrada: 'asdf', csrfmiddlewaretoken: 'asdf'};
-      nock(domain)
-        .get('/')
-        .replyWithFile(200, path.join(__dirname, 'form.html'))
-        .post('/get_generic_ajax/', form)
-        .reply(200, {status: 'fail', value: []});
-    });
-    it('should return a empty results', done => {
-      lib.getEnterpriseRut('asdf').then(results => {
-        expect(results).to.eql([]);
-        done();
-      });
-    });
-  });
-});
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: 'asdf' })
+        .reply(200, [])
+    })
+    it('should return a empty results', async () => {
+      const results = await lib.getEnterpriseRut('asdf')
+      expect(results).to.eql([])
+    })
+  })
+
+  describe('server error', () => {
+    beforeEach(() => {
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: 'asdf' })
+        .reply(500)
+    })
+    it('should return a empty results', async () => {
+      try {
+        await lib.getEnterpriseRut('asdf')
+      } catch (err) {
+        expect(err).to.be.an('error')
+      }
+    })
+  })
+
+  describe('json error', () => {
+    beforeEach(() => {
+      nock('https://api.rutify.cl')
+        .get('/search')
+        .query({ q: 'asdf' })
+        .reply(200, 'bad json')
+    })
+    it('should return a empty results', async () => {
+      try {
+        await lib.getEnterpriseRut('asdf')
+      } catch (err) {
+        expect(err).to.be.an('error')
+      }
+    })
+  })
+})
