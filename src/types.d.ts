0ecaade1d1607be7a6c584055ad8aa6c3cb5e84e
Leonardo Gatica <lgatica@protonmail.com>
2019-02-20 11:31:41 -0300

    style(typescript): apply tslint

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,41 +1,41 @@
-export type Params = {
-  type: string;
-  key: string;
-  term: string;
-};
+export interface Params {
+  type: string
+  key: string
+  term: string
+}
 
-export type Person = {
-  name: string;
-  rut: string;
-  gender: string;
-  address: string;
-  city: string;
-};
+export interface Person {
+  name: string
+  rut: string
+  gender: string
+  address: string
+  city: string
+}
 
-export type Enterprise = {
-  name: string;
-  item: string;
-  subitem: string;
-  activity: string;
-  rut: string;
-};
+export interface Enterprise {
+  name: string
+  item: string
+  subitem: string
+  activity: string
+  rut: string
+}
 
-export type Payload = Person | Enterprise;
+export type Payload = Person | Enterprise
 
-export function getData(params: Params): Promise<Payload[]>;
+export function getData (params: Params): Promise<Payload[]>
 
-export function getPersonByRut(rut: string): Promise<Person>;
+export function getPersonByRut (rut: string): Promise<Person>
 
-export function getEnterpriseByRut(rut: string): Promise<Enterprise>;
+export function getEnterpriseByRut (rut: string): Promise<Enterprise>
 
-export function reverse(name: string): string;
+export function reverse (name: string): string
 
-export function titleize(name: string): string;
+export function titleize (name: string): string
 
-export function fuzzzySearch(name: string, list: Payload[]): Payload[];
+export function fuzzzySearch (name: string, list: Payload[]): Payload[]
 
-export function getRut(name: string, type: string): Promise<Payload[]>;
+export function getRut (name: string, type: string): Promise<Payload[]>
 
-export function getPersonByName(name: string): Promise<Person[]>;
+export function getPersonByName (name: string): Promise<Person[]>
 
-export function getEnterpriseByName(name: string): Promise<Enterprise[]>;
+export function getEnterpriseByName (name: string): Promise<Enterprise[]>
